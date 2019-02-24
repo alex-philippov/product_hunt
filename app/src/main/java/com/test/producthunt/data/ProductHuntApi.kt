036c1b1342b803ccdc6679b8e43ff5720a63eb48
phisss <filalx2013@gmail.com>
2019-02-24 20:48:34 +0300

    change products request to get products for all time

--- app/src/main/java/com/test/producthunt/data/ProductHuntApi.kt
+++ app/src/main/java/com/test/producthunt/data/ProductHuntApi.kt
@@ -3,11 +3,12 @@ package com.test.producthunt.data
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ProductHuntApi {
 
-    @GET("categories/{category}/posts")
-    fun products(@Path("category") category: String) : Deferred<PostsResponse>
+    @GET("posts/all?")
+    fun products(@Query("search[category]") category: String) : Deferred<PostsResponse>
 
     @GET("topics")
     fun topics() : Deferred<TopicsResponse>
--- app/src/main/java/com/test/producthunt/presentation/productslist/ProductsFragment.kt
+++ app/src/main/java/com/test/producthunt/presentation/productslist/ProductsFragment.kt
@@ -4,13 +4,10 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.PopupMenu
-import android.widget.TextView
-
 import com.test.producthunt.R
 import com.test.producthunt.domain.Product
 import com.test.producthunt.domain.Topic
@@ -48,7 +45,6 @@ class ProductsFragment : Fragment() {
         rv_products.adapter = adapter
 
         viewModel.productsStore.observe(this) {
-            Log.d("HUI DEBUG", it.toString())
             when (it) {
                 is Lce.Success -> successProducts(it.data)
                 is Lce.Error -> error()
@@ -56,7 +52,6 @@ class ProductsFragment : Fragment() {
             }
         }
         viewModel.topicsStore.observe(this) {
-            Log.d("HUI DEBUG", it.toString())
             when (it) {
                 is Lce.Success -> successTopics(it.data)
                 is Lce.Error -> error()
